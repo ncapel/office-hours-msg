aa622a1d5e06c086e3f08224b61c44976c6018fd
ncapel <124222256+ncapel@users.noreply.github.com>
2024-08-29 20:05:22 -0400

    init commit

---
+++ sendMessage.js
@@ -0,0 +1,22 @@
+const { WebhookClient, roleMention } = require('discord.js');
+
+const webhookId = process.env.WEBHOOK_ID;
+const webhookToken = process.env.WEBHOOK_TOKEN;
+
+const webhookClient = new WebhookClient({
+	id: webhookId,
+	token: webhookToken,
+});
+
+const roleId = process.env.ROLE_ID;
+const roleMent = roleMention(roleId);
+
+function sendMessage() {
+	webhookClient
+		.send(
+			`Good Morning! ${roleMent} Office Hours will be held this Wednesday and Thursday following our class session. No addition zoom links are needed, just stick around after class has concluded.`
+		)
+		.catch(console.error);
+}
+
+sendMessage();
